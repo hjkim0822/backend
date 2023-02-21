28583e40cf5e1924cdaa75f5d99469df95f0daec
김형준 <hjkim0822@gmail.com>
2023-02-21 18:49:27 +0900

    doing something

---
+++ src2/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
